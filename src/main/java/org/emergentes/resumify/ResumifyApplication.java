035e57c5aa3b7ad254aa4f0742ecdac48b171fc2
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2024-05-06 16:24:14 -0500

    Initial Commit

---
+++ src/main/java/org/emergentes/resumify/ResumifyApplication.java
@@ -0,0 +1,13 @@
+package org.emergentes.resumify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/emergentes/resumify/ResumifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emergentes.resumify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
