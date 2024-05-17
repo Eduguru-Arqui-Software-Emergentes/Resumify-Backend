21d840cda9a8e4545368f949295548fe3a584097
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2024-05-17 12:01:40 -0500

    fix: Add CrossOrigin by CORS

--- src/main/java/org/emergentes/resumify/controller/AuthenticationController.java
+++ src/main/java/org/emergentes/resumify/controller/AuthenticationController.java
@@ -9,16 +9,14 @@ import org.emergentes.resumify.util.Message;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/autenticacion")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
